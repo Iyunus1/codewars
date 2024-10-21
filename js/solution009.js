550e4f4e281496efe301458f4d6a3c46c4ec5325
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2024-10-21 15:50:26 +0100

    solution009 completed

---
+++ js/solution009.js
@@ -0,0 +1,8 @@
+const capitalize = (string) => {
+  return string
+    .split(" ")
+    .map((word) => word[0].toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+console.log(capitalize("How can mirrors be real if our eyes aren't real"));
