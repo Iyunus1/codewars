abd6ac0e6d3a1f603069eacd87546e76e9bf2c36
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2024-10-22 18:30:45 +0100

    solution010 completed

--- js/solution009.js
+++ js/solution009.js
@@ -1,8 +1,7 @@
-const capitalize = (string) => {
-  return string
+const capitalize = (string) =>
+  string
     .split(" ")
     .map((word) => word[0].toUpperCase() + word.slice(1))
     .join(" ");
-};
 
 console.log(capitalize("How can mirrors be real if our eyes aren't real"));
---
+++ js/solution010.js
@@ -0,0 +1,5 @@
+const num = 5;
+
+const opposite = (number) => -number;
+
+console.log(opposite(num));
