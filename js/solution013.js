28c979055e92541c89fb73d69c61bde8579f028c
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2024-10-25 13:53:48 +0100

    solution013 completed

---
+++ js/solution013.js
@@ -0,0 +1,27 @@
+const XO = (str) => {
+  let x = [];
+  let o = [];
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i].toLowerCase() === "x") {
+      x.push(string[i].toLowerCase());
+    } else if (string[i].toLowerCase() === "o") {
+      o.push(string[i].toLowerCase());
+    }
+  }
+
+  if (x.length === o.length) {
+    return true;
+  } else if (
+    x.length > o.length ||
+    x.length < o.length ||
+    o.length < x.length ||
+    o.length < x.length
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+console.log(XO("asdp"));
