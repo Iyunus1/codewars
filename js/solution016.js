8b4f517948b660ef139deb2a5e547ec42257c6e0
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2024-10-29 12:59:13 +0000

    solution017 completed

--- js/solution016.js
+++ js/solution016.js
@@ -2,9 +2,10 @@
 // Use gauss thereom as the input can be converted into a contigous array
 
 const getSum = (a, b) => {
-  let min = Math.min(a, b),
-    max = Math.max(a, b);
+  let min = Math.min(a, b), // 4
+    max = Math.max(a, b); // 7
   return ((max - min + 1) * (min + max)) / 2;
+  // 7 - 4, 3 + 1 * 4 + 7
 };
 
 console.log(getSum(4, 7));
---
+++ js/solution017.js
@@ -0,0 +1,4 @@
+const reverseWords = (str) =>
+  str.split("").reverse().join("").split(" ").reverse().join(" ");
+
+console.log(reverseWords(".god yzal eht revo spmuj xof nworb kciuq ehT"));
