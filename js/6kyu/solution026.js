01656a5a985c6cad3ad8d2ef35de6be7f495b5e8
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2024-11-07 14:07:56 +0000

    solution026 completed

---
+++ js/6kyu/solution026.js
@@ -0,0 +1,7 @@
+function createPhoneNumber(numbers){
+    let format = '(xxx) xxx-xxxx'
+    numbers.map((num) => format = format.replace('x', num))
+    return format;
+}
+
+console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
