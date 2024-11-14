5bb45cadde1a8431d44d4f91526bd968edc26cbf
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2024-11-14 14:32:13 +0000

    solution033 completed

---
+++ js/8kyu/solution033.js
@@ -0,0 +1,6 @@
+// Contigous array and the output is a sum number of all numbers added up.
+// So gauss theory applies here
+
+const summation = num => num * (num + 1) / 2
+
+console.log(summation(8))
