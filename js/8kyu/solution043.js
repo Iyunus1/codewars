2e331489b3fcd976751ef4de6ae723953cafedb0
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2024-11-23 19:52:02 +0000

    solution043 completed

---
+++ js/8kyu/solution043.js
@@ -0,0 +1,6 @@
+
+const makeUpperCase = str => str.toUpperCase()
+const loopUpper = str => str.split("").map((element) => element.toUpperCase()).join("") // Convert string into array and
+
+console.log(loopUpper('Supasdasd'))
+
