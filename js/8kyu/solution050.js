94f9d899c50f9be22e9dada2e1410d0e6a7ae443
Iyunus1 <90559412+Iyunus1@users.noreply.github.com>
2025-01-15 16:00:00 +0000

    Solution 090 completed

---
+++ js/8kyu/solution050.js
@@ -0,0 +1,12 @@
+const quarterOf = (month) => {
+
+    if(month >= 1 && month < 4){
+      return 1
+    } else if(month >= 4 && month < 7){
+      return 2
+    } else if(month >= 7 && month < 10){
+      return 3
+    } else if(month >= 10 && month < 13){
+      return 4
+    }
+  }
